62d242ac6442f4c6e5ef4de3f6749f7d0519ac6c
sojeongsojeong-park <psj90972@naver.com>
2022-07-03 16:00:37 +0900

    feat: uniRecord로 데이터를 보내기 위한 코드 작업 uniRecord로 데이터를 보내기 위한 클릭이벤트 코드를 추가하였습니다.

--- src/views/myRecord/myRecord.js
+++ src/views/myRecord/myRecord.js
@@ -1,45 +1,43 @@
-const main = document.querySelector('main');
-const emptyModal = document.querySelector('.emptyListModal');
+const main = document.querySelector("main");
+const emptyModal = document.querySelector(".emptyListModal");
 
-const recordLists = document.createElement('ol');
+const recordLists = document.createElement("ol");
 
-for(let i = 1; i <= localStorage.length; i++) {
-    const bookTitleKey = JSON.parse(localStorage.getItem(i));
-    const bookTitle = bookTitleKey.title;
-    makeLists(bookTitle);
+for (let i = 1; i <= localStorage.length - 2; i++) {
+  const bookTitleKey = JSON.parse(localStorage.getItem(i));
+  const bookTitle = bookTitleKey.title;
+  makeLists(bookTitle, bookTitleKey);
 }
 
-console.log(localStorage.getItem(1));
-
-if(!localStorage.getItem(1)) {
-    emptyModal.classList.remove('emptyLists');
+if (!localStorage.getItem(1)) {
+  emptyModal.classList.remove("emptyLists");
 } else {
-    emptyModal.classList.add('emptyLists');
+  emptyModal.classList.add("emptyLists");
 }
 
+function makeLists(bookTitle, bookTitleKey) {
+  const recordList = document.createElement("li");
+  const recordListTitle = document.createElement("strong");
+  const icons = document.createElement("div");
+  const writeIcon = document.createElement("i");
+  const deleteIcon = document.createElement("i");
+  const value = JSON.stringify(bookTitleKey);
 
-function makeLists(bookTitle) {
-    const recordList = document.createElement('li');
-    const recordListTitle = document.createElement('strong');
-    const icons = document.createElement('div');
-    const writeIcon = document.createElement('i');
-    const deleteIcon = document.createElement('i');
+  recordLists.classList.add("recordLists");
+  recordListTitle.classList.add("recordListTitle");
+  recordListTitle.textContent = bookTitle;
+  icons.classList.add("icons");
+  writeIcon.classList.add("fa-solid");
+  writeIcon.classList.add("fa-pencil");
+  deleteIcon.classList.add("fa-solid");
+  deleteIcon.classList.add("fa-trash-can");
 
-    recordLists.classList.add('recordLists');
-    recordListTitle.classList.add('recordListTitle');
-    recordListTitle.textContent = bookTitle;
-    icons.classList.add('icons');
-    writeIcon.classList.add('fa-solid');
-    writeIcon.classList.add('fa-pencil');
-    deleteIcon.classList.add('fa-solid');
-    deleteIcon.classList.add('fa-trash-can');
+  recordList.addEventListener("click", () => {
+    localStorage.setItem("showItem", value);
+  });
 
-    icons.append(writeIcon, deleteIcon);
-    recordList.append(recordListTitle, icons);
-    recordLists.append(recordList);
-    main.append(recordLists);
+  icons.append(writeIcon, deleteIcon);
+  recordList.append(recordListTitle, icons);
+  recordLists.append(recordList);
+  main.append(recordLists);
 }
-
-
-
-
